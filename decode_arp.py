a6a3a55699921093440dbdf09a871309c746bd9e
Jeff Silverman <jeffsilverm@gmail.com>
2011-04-26 20:02:07 -0700

    decode_arp.py is now decoding IP addresses and MAC addresses

--- decode_arp.py
+++ decode_arp.py
@@ -6,6 +6,8 @@ import sys
 import socket
 import pcap
 import subprocess
+# import dnet     # used to translate Ethernet addresses from packed 48 bit number to human readable format
+import binascii
 
 hostname = subprocess.Popen("hostname", stdout=subprocess.PIPE).communicate()[0]
 if hostname == 'jeffs-laptop\n' :
@@ -17,8 +19,20 @@ pc.setfilter('arp')         # Use a kernel filter and just pass arp traffic
 for ts, pkt in pc:
     # parse the packet.  Because the filter allows only ARP packets through, we don't have to decode the ethertype
     eth = dpkt.ethernet.Ethernet(pkt)
-    arp = dpkt.arp(eth.data)
+    arp = eth.arp
     print arp
+    print arp.hrd
+    print arp.pro
+    if arp.op==1 :
+        print "request"
+    elif arp.op==2 :
+	print "reply"
+    else :
+	print "op has an unexpected value %d", arp.op
+    print "Target protocol address", socket.inet_ntoa(arp.tpa)	#IPv4 address
+    print "target hardware address", binascii.hexlify(arp.tha)
+
+
 
     
 
