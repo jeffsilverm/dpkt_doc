324eef491b61a0aaa1eeed11d0f6d5621cd08968
Jeff Silverman <jeffsilverm@gmail.com>
2011-04-26 20:57:16 -0700

    More updates to decode_arp.py - MAC addresses are colon separated

--- decode_arp.py
+++ decode_arp.py
@@ -6,31 +6,40 @@ import sys
 import socket
 import pcap
 import subprocess
-# import dnet     # used to translate Ethernet addresses from packed 48 bit number to human readable format
 import binascii
+
+def add_colons_to_mac( mac_addr ) :
+    """This function accepts a 12 hex digit string and converts it to a colon
+separated string"""
+    s = list()
+    for i in range(12/2) : 	# mac_addr should always be 12 chars, we work in groups of 2 chars
+        s.append( mac_addr[i*2:i*2+2] )
+    r = ":".join(s)		# I know this looks strange, refer to http://docs.python.org/library/stdtypes.html#sequence-types-str-unicode-list-tuple-bytearray-buffer-xrange
+    return r
 
 hostname = subprocess.Popen("hostname", stdout=subprocess.PIPE).communicate()[0]
 if hostname == 'jeffs-laptop\n' :
-    pc = pcap.pcap('wlan0')       # set up for packet capture
+    pc = pcap.pcap('wlan0', promisc=True)       # set up for packet capture
 else:
-    pc = pcap.pcap('eth0')
+    pc = pcap.pcap('eth0', promisc=True)
 pc.setfilter('arp')         # Use a kernel filter and just pass arp traffic
 
 for ts, pkt in pc:
     # parse the packet.  Because the filter allows only ARP packets through, we don't have to decode the ethertype
     eth = dpkt.ethernet.Ethernet(pkt)
     arp = eth.arp
-    print arp
-    print arp.hrd
-    print arp.pro
+#    print arp.hrd
+#    print arp.pro
     if arp.op==1 :
         print "request"
     elif arp.op==2 :
 	print "reply"
     else :
 	print "op has an unexpected value %d", arp.op
+    print "source protocol address", socket.inet_ntoa(arp.spa)
+    print "source hardware address", add_colons_to_mac( binascii.hexlify(arp.sha) )
     print "Target protocol address", socket.inet_ntoa(arp.tpa)	#IPv4 address
-    print "target hardware address", binascii.hexlify(arp.tha)
+    print "target hardware address", add_colons_to_mac( binascii.hexlify(arp.tha) )
 
 
 
