cb536b674fb563c10904f01cb24600d876a3dca1
Jeff SIlverman <jeffsilverm@gmail.com>
2011-05-15 17:49:40 -0700

    Changed the command line argument list to support files

--- decode_udp.py
+++ decode_udp.py
@@ -18,6 +18,7 @@ def decode_udp ( pc ) :
         if eth.type == dpkt.ethernet.ETH_TYPE_IP :
             ip = eth.data
             if ip.p == dpkt.ip.IP_PROTO_UDP :
+# This doesn't deal with IP fragments                
                 udp = ip.data
                 # Pass the IP addresses, source port, destination port, and data back to the caller.
                 yield ( ip.src, udp.sport, ip.dst, udp.dport, udp.data)
@@ -26,12 +27,20 @@ def decode_udp ( pc ) :
 def main() :
 # This code allows this program to run equally well on my laptop and my desktop.  I did it this
 # way to demonstrate different interface names.  If I was really clever, I'd figure out how to do  this
-# under MS-Windows
-    hostname = subprocess.Popen("hostname", stdout=subprocess.PIPE).communicate()[0]
-    if hostname == 'jeffs-laptop\n' :
-        pc = pcap.pcap('wlan0', promisc=True)       # set up for packet capture
-    else:
-        pc = pcap.pcap('eth0', promisc=True)
+# under MS-Windows 
+#    hostname = subprocess.Popen("hostname", stdout=subprocess.PIPE).communicate()[0]
+#    if hostname == 'jeffs-laptop\n' :
+#        pc = pcap.pcap('wlan0', promisc=True)       # set up for packet capture
+#    else:
+#        pc = pcap.pcap('eth0', promisc=True)
+    if sys.argv[1] == "-i" :
+        pc = pcap.pcap( sys.argv[2] )
+    elif sys.argv[1] == "-f" :
+        pc = dpkt.pcap.Reader( open ( sys.argv[2] ) )
+    else :
+        print """Use -i INTERFACE to [packet capture from an interface.
+Use -f FILENAME to read a packet capture file"""
+        sys.exit(2)
 
     for src, sport, dst, dport, data in decode_udp( pc ) :
         print "from ", socket.inet_ntoa(src),":",sport, " to ", socket.inet_ntoa(dst),":",dport
