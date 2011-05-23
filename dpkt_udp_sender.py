e4b24708fb88bd51a7eaeb2acb5d9af7b36339d4
Jeff SIlverman <jeffsilverm@gmail.com>
2011-05-22 22:52:26 -0700

    This doesn't work, and needs some TLC

--- dpkt_udp_sender.py
+++ dpkt_udp_sender.py
@@ -23,10 +23,11 @@ def help() :
 
 def parse_args ( ) :
     """Parse the command line arguments.  The arguments are:
--6        Use IPv6 (default is IP4) (must come before -d if ipV6)
+-6        Use IPv6 (default is IPv4) (must come before -d if IPv6)
 -d IP     destination IP address (either IPv4 or IPv6) (required)
--s size   size of the UDP packet (0 < size < 65536, upper limit is not enforced, default is 4096
+-l length size of the UDP packet (0 < size < 65536, upper limit is not enforced, default is 4096
 -p port   destination port (default is 50005, 0< port < 65536)
+-s port   source port (default is 50004, 0< port < 65536)
 -h        help
 """
     try :
@@ -34,17 +35,22 @@ def parse_args ( ) :
         ipv6 = 0
         destination_ip = ""
         destination_port = 50005
-        message_size = 4096
+        source_port = 50004
+        message_length = 4096
         for opt in optlist :
             if opt[0] == "-6" : ipv6 = 1
             elif opt[0] == "-d" :
                destination_ip = opt[1]
-            elif opt[0] == "-s" :
-                message_size = int( opt[1] )
+            elif opt[0] == "-l" :
+                message_length = int( opt[1] )
             elif opt[0] == "-p" :
                 destination_port = int( opt[1] )
                 if not ( 0 < destination_port < 65536 ) :
                     raise ValueError 
+            elif opt[0] == "-s" :
+                source_port = int ( opt[1] )
+                if not ( 0 < destination_port < 65536 ) :
+                    raise ValueError                
             elif opt[0] == "-h" :
                 help()
         if destination_ip == ""  :
@@ -52,15 +58,15 @@ def parse_args ( ) :
             raise ValueError
     except ( getopt.GetoptError, ValueError ) :
         help()
-    return (destination_ip, destination_port, message_size, ipv6)
+    return (destination_ip, destination_port, source_port, message_length, ipv6)
 
 
-def create_udp_packet (destination_port, message_size, ipv6) :
+def create_udp_packet (destination_ip, destination_port, source_port, message_length, ipv6) :
     udp = dpkt.udp.UDP()
-    udp.sport = 50004               # UDP source port, doesn't really matter
+    udp.sport =  source_port
     udp.dport = destination_port
-    udp.ulen = message_size + 8		# add 8 because that's the length of the UDP header
-    message = message_size * "*"
+    udp.ulen = message_length + 8		# add 8 because that's the length of the UDP header
+    message = message_length * "*"
     udp.data = message
 # Need to calculate a UDP checksum
     return udp.pack()
@@ -70,9 +76,10 @@ def createRawIPSocket( ipv6 ):
     return sock
 
 if __name__ == "__main__" :
-    (destination_ip, destination_port, message_size, ipv6) = parse_args( )
+    ( destination_ip, destination_port, source_port, message_length, ipv6) = parse_args( )
     sock = createRawIPSocket( ipv6 )
-    udp_packet = create_udp_packet ( destination_port, message_size, ipv6)
+# create_udp_packet needs the destination IP address so it can calculate the UDP checksum    
+    udp_packet = create_udp_packet ( destination_ip, destination_port, source_port, message_length, ipv6)
     sock.sendto(udp_packet, (destination_ip, destination_port ) )
 
 
