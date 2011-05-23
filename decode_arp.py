3240645000228bdc8a3f93d5e7c35f8b1145525f
Jeff SIlverman <jeffsilverm@gmail.com>
2011-05-22 22:43:57 -0700

    Fixed the documentation on how to convert an Ethernet address to ASCII

--- decode_arp.py
+++ decode_arp.py
@@ -19,48 +19,59 @@ separated string"""
     return r
 
 
-# This code allows this program to run equally well on my laptop and my desktop.  I did it this
-# way to demonstrate different interface names.  If I was really clever, I'd figure out how to this
-# under MS-Windows
-hostname = subprocess.Popen("hostname", stdout=subprocess.PIPE).communicate()[0]
-if hostname == 'jeffs-laptop\n' :
-    pc = pcap.pcap('wlan0', promisc=True)       # set up for packet capture
-else:
-    pc = pcap.pcap('eth0', promisc=True)
-pc.setfilter('arp')         # Use a kernel filter and just pass arp traffic
-start_time = time.time()    # Return the time as a floating point number expressed in seconds since the epoch
-arp_cache = dict()
-
-# a pcap.pcap object listens to the network and returns a packet object when it hears a packet.
-for ts, pkt in pc:
-    # parse the packet.  Because the filter allows only ARP packets through, we don't have to decode the ethertype
-    eth = dpkt.ethernet.Ethernet(pkt)
-    arp = eth.arp
-#    print arp.hrd
-#    print arp.pro
-    if arp.op==1 :
-        print "request"
-    elif arp.op==2 :
-	print "reply"
+def main() :
+    # This code allows this program to run equally well on my laptop and my desktop.  I did it this
+    # way to demonstrate different interface names.  If I was really clever, I'd figure out how to this
+    # under MS-Windows
+    if sys.argv[1] == "-i" :
+        pc = pcap.pcap( sys.argv[2] )
+    elif sys.argv[1] == "-f" :
+        pc = dpkt.pcap.Reader( open ( sys.argv[2] ) )
     else :
-	print "op has an unexpected value %d", arp.op
-    print "source protocol address", socket.inet_ntoa(arp.spa)
-    print "source hardware address", add_colons_to_mac( binascii.hexlify(arp.sha) )
-    print "Target protocol address", socket.inet_ntoa(arp.tpa)	#IPv4 address
-    print "target hardware address", add_colons_to_mac( binascii.hexlify(arp.tha) )
-    arp_cache[arp.spa] = arp.sha
-    if arp.op==2 :
-        arp_cache[arp.tpa] = arp.tha
-# The arp cache has to be refreshed every 30 seconds, so this will catch every machine on the ethernet at least twice.  However, you might want to
-# continuously monitor your network to catch machines surreptitiously.
-    elapsed_time = time.time() - start_time
-    if elapsed_time > 60.0 :
-        break
+        print """Use -i INTERFACE to [packet capture from an interface.
+Use -f FILENAME to read a packet capture file"""
+        sys.exit(2)
 
-for ip in arp_cache.keys() :
-    print socket.inet_ntoa(ip),": ", add_colons_to_mac( binascii.hexlify(arp_cache[ip]))
+    pc.setfilter('arp')         # Use a kernel filter and just pass arp traffic
+    start_time = time.time()    # Return the time as a floating point number expressed in seconds since the epoch
+    arp_cache = dict()
+
+    # a pcap.pcap object listens to the network and returns a packet object when it hears a packet.
+    for ts, pkt in pc:
+        # parse the packet.  Because the filter allows only ARP packets through, we don't have to decode the ethertype
+        eth = dpkt.ethernet.Ethernet(pkt)
+        arp = eth.arp
+    #    print arp.hrd
+    #    print arp.pro
+        if arp.op==1 :
+            print "request"
+        elif arp.op==2 :
+            print "reply"
+        else :
+            print "op has an unexpected value %d", arp.op
+        print "source protocol address", socket.inet_ntoa(arp.spa)
+        print "source hardware address", add_colons_to_mac( binascii.hexlify(arp.sha) )
+        print "Target protocol address", socket.inet_ntoa(arp.tpa)	#IPv4 address
+        print "target hardware address", add_colons_to_mac( binascii.hexlify(arp.tha) )
+        arp_cache[arp.spa] = arp.sha
+        if arp.op==2 :          # reply
+            if arp.tpa in arp_cache :
+                if arp_cache[arp.tpa] != arp.tha :
+                    print "Duplicate IP addresses detected: ", socket.inet_ntoa(arp.tpa), "is assigned to both ",
+                    add_colons_to_mac( binascii.hexlify(arp.tha) ), " and ", add_colons_to_mac( binascii.hexlify(arp_cache[arp.tha] ) )
+            arp_cache[arp.tpa] = arp.tha
+    # The arp cache has to be refreshed every 30 seconds, so this will catch every machine on the ethernet at least twice.  However, you might want to
+    # continuously monitor your network to catch machines surreptitiously.
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 60.0 :
+            break
+
+    for ip in arp_cache.keys() :
+        print socket.inet_ntoa(ip),": ", add_colons_to_mac( binascii.hexlify(arp_cache[ip]))
 
     
+if __name__ == "__main__" :
+    main()
     
 
 
